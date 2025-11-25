5c58b9b4f23e2f148ea228fca6de08a73eb8e160
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-11-26 00:34:52 +0530

    Update tailwind.config.cjs

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -4,17 +4,24 @@ module.exports = {
     './index.html',
     './src/**/*.{js,jsx,ts,tsx,html}'
   ],
+  safelist: [
+    'text-course-deatails-heading-small',
+    'md:text-course-deatails-heading-large'
+  ],
   theme: {
     extend: {
       fontSize: {
-        'course-details-heading-small' : ['26px', '36px'],
-        'course-details-heading-large' : ['36px', '44px'],
+        'course-deatails-heading-small' : ['26px', '36px'],
+        'course-deatails-heading-large' : ['36px', '44px'],
         'home-heading-small' : ['28px', '34px'],
         'home-heading-large' : ['48px', '56px'],
         'defult' : ['15px', '21px']
       },
       gridTemplateColumns: {
         'auto' : 'repeat(auto-fit, minmax(200px, 1fr))'
+      },
+      spacing: {
+        'section-height' : '500px',
       }
     },
   },
