7784b40551a6c5b99a25e1968c25f96bbee6d620
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-06 01:29:59 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/User.js"
+import Stripe from "stripe";
+import { Purchase } from "../models/Purchase.js";
 
 // Get User data
 export const getUserData = async (req, res)=>{
@@ -25,6 +27,36 @@ export const userEnrolledCourses = async (req, res)=>{
         res.json({success: true, enrolledCourses: userData.enrolledCourses})
     } catch (error) {
         res.json({success: false, message: error.message})
+    }
+}
+
+// Purchase Course
+export const purchaseCourse = async (req, res) =>{
+    try {
+      const {courseId} = req.body
+      const { origin } = req.headers
+      const userId = req.auth.userId
+      const userData = await User.findById(userId)
+      const courseData = await Course.findById(courseId)
+
+      if(!userData || !courseData){
+        return res.json({success: false, message: 'Data not found'})
+      }
+
+      const purchaseData = {
+        courseId: courseData._id,
+        userId,
+        amount: (courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed (2),
+      }
+
+      const newPurchase = await Purchase.create(purchaseData)
+
+      // Stripe Gateway Initialize
+      const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+      
+
+    } catch (error) {
+      
     }
 }
 
