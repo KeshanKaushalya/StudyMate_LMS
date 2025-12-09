cb4594776619640c8720bb591b36f5bd43b87cbd
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-09 12:12:56 +0530

    Update Footer.jsx

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -12,10 +13,10 @@ const Footer = () => {
         <div className='flex flex-col md:items-start items:center w-full'>
           <h2 className='font-semibold text-white mb-5'>Company</h2>
           <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>About us</a></li>
-            <li><a href='#'>Contact us</a></li>
-            <li><a href='#'>Privacy policy</a></li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/about'>About us</Link></li>
+            <li><Link to='/contact'>Contact us</Link></li>
+            <li><Link to='/privacy'>Privacy policy</Link></li>
           </ul>
         </div>
         <div className='hidden md:flex flex-col items-start w-full'>
