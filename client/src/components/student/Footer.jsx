6f97ba85538f35d35f760693542aba3ade1af53e
Keshan Kaushalya <madusankakeshan782@gmail.com>
2025-12-18 16:14:50 +0530

    Update Footer.jsx

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className='bg-gray-900 md:px-36 text-left w-full mt-10'>
       <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
         <div className='flex flex-col md:items-start items:center w-full'>
-          <img src={assets.logo_dark} alt="logo" />
+          <img src={assets.logo3} alt="logo" className='w-50'/>
           <p className='mt-6 text-center md:text-left text-sm text-white/80'>StudyMate is a modern learning platform that helps students learn smarter with engaging tools, personalized guidance, and a supportive environment designed to improve academic success.</p>
         </div>
         <div className='flex flex-col md:items-start items:center w-full'>
